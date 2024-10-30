28dfada83a0d274ececc0d8c72cbd541163e4dc6
keithchasen <keithchasen89@gmail.com>
2024-10-30 21:20:12 +0100

    basic user service set up

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { DbModule } from 'src/db/db.module';
 import { UsersService } from './users.service';
 
 @Module({
-  providers: [UsersService]
+  imports: [DbModule], // since service uses db module
+  providers: [UsersService],
+  exports: [UsersService], // so we can use it in other modules
 })
 export class UsersModule {}
